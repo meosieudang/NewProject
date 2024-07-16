85b1e4c3313aaf63c245a8a27ae00699ca362d38
thuanhn <thuanhn@creasia.com.vn>
2024-07-16 14:17:09 +0700

    add new icons

---
+++ src/assets/index.ts
@@ -0,0 +1,12 @@
+const bg_red = require('./bg/bg_red.png');
+const close = require('./icons/close.png');
+const check = require('./icons/check.png');
+const warning = require('./icons/warning.png');
+const kpi = require('./icons/kpi.png');
+const idea = require('./icons/idea.png');
+const loading = require('./icons/loading.png');
+const smartCity = require('./icons/smart-city.png');
+const leftArrow = require('./icons/left-arrow.png');
+const caretDown = require('./icons/caret-down.png');
+
+export { loading, caretDown, leftArrow, warning, bg_red, close, check, kpi, idea, smartCity };
