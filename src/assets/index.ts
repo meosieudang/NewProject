0ce1c66407899a53306650e0ac3df45b8124d479
thuanhn <thuanhn@creasia.com.vn>
2024-07-19 15:27:08 +0700

    add assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,4 @@
-const bg_red = require('./bg/bg_red.png');
+const bgApp = require('./icons/bgLogin.png');
 const close = require('./icons/close.png');
 const check = require('./icons/check.png');
 const warning = require('./icons/warning.png');
@@ -9,4 +9,4 @@ const smartCity = require('./icons/smart-city.png');
 const leftArrow = require('./icons/left-arrow.png');
 const caretDown = require('./icons/caret-down.png');
 
-export { loading, caretDown, leftArrow, warning, bg_red, close, check, kpi, idea, smartCity };
+export { loading, caretDown, leftArrow, warning, bgApp, close, check, kpi, idea, smartCity };
